76cd596bda905b3582a3ea261aa8c3ea9dce1dd0
hudodobird <hudster8@icloud.com>
2025-09-15 10:29:48 +0100

    Django forms for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,42 +1,22 @@
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == "POST":
-        username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
-
-        if User.objects.filter(username=username):
-            messages.error(request, "Username already exists! Please try some other username")
-            return render(request, 'accounts/signup.html')
-
-        if User.objects.filter(email=email):
-            messages.error(request, "Email already registered!")
-            return render(request, 'accounts/signup.html')
-
-        if len(username) > 50:
-            messages.error(request, "Username must be under 50 characters")
-
-        if pass1 != pass2:
-            messages.error(request, "Passwords didn't match!")
-
-        if not username.isalnum():
-            messages.error(request, "Username must be letters or numbers!")
-
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name = fname
-        myuser.last_name = lname
-        myuser.save()
-        messages.success(request, "Account successfully created.")
-        return redirect('home')
-    return render(request, 'accounts/signup.html')
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Account successfully created.")
+            return redirect('home')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 @login_required
 def profile(request):
