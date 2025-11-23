135a9f586a3b6d1ebf784c90d8dcc7f989a34281
hudodobird <159005081+hudodobird@users.noreply.github.com>
2025-11-23 13:25:23 +0000

    Fixed images loading issue

--- my_website/urls.py
+++ my_website/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('tutorial/', include('tutorial.urls')),
     path('pastpapers/', include('pastpapers.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
